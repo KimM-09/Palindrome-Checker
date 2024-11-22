6b2c4e37049a9ea288b6adec2574e7f3a9f8f4fd
Kim McLane <kmmclan09@gmail.com>
2024-11-22 15:49:32 -0500

    added README, commented JS

---
+++ README.md
@@ -0,0 +1,8 @@
+# Palindrome checker.
+
+## What is a palindrome?
+A palindrome is a word or phrase that is spelled the same forwards and backwards.
+Examples of a palindrome include 'Taco cat', 'Otto', 'A man, a plan, a canal. Panama.'
+
+## Check it out!
+https://kimm-09.github.io/Palindrome-Checker/
--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const checker = document.getElementById("checker");
 
 const checkPalindrome = str => {
   const orgStr = str;
+  //check that the input is not empty
   if(str === '') {
     alert('Please input a value');
     return;
@@ -15,7 +16,10 @@ const checkPalindrome = str => {
    results.replaceChildren();
 
  const regex = /[+-\s!@#$%^&*()_+=\[\]{};':"\\|,.<>\/?~`]/g;
+ //make the string lowercase, check the string for the characters from the regex and replace them with an empty space; assign that value to lowerCaseStr
  const lowerCaseStr = str.replace(regex, '').toLowerCase();
+ /*if the value of lowerCaseStr strictly equals the revers of lowerCaseStr, ${orgStr} is a palindrome, 
+ else ${orgStr} is not a palindrome; assign that value to resultsMsg*/
  let resultsMsg = `${orgStr} ${lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'} a palindrome.`;
 
  const pTag = document.createElement('p');
